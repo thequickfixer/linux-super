88654cd6d1a38912c8991744840b5db60c0e512f
thequickfixer <thequickfixer@users.noreply.github.com>
2022-04-08 09:06:45 -0400

    improve run.sh

--- run.sh
+++ run.sh
@@ -21,22 +21,34 @@ read -p "> " kernelver
 
 if [ $kernelver == 5.14.21 ]; then
     echo "Download?"
+    read -p "> " _input
+else
+# if not 5.14.21 do:
+    echo "Attempting to download kernel $kernelver"
+    echo "Download?"
     read -p "> " input
-    if [ $input == y ]; then
+	if [[ $input == y ]]; then
+        	mkdir linux-super-work
+        	cd $workdir
+        	wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$kernelver.tar.xz
+        	else
+		echo "user returned $input"
+        	fi
+fi
+    if [[ $_input == y ]]; then
         mkdir linux-super-work
         cd $workdir
         wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$kernelver.tar.xz
-        if [ ! -d "$workdir/linux-$kernelver.tar.xz" ]; then
-            echo "no kernel version"
         else
+	echo "user returned $_input"
+	fi
+	if [ ! -d "$workdir/linux-$kernelver.tar.xz" ]; then
+            echo "no kernel version"
+	    fi
+	else
         echo "Extract the files?"
         if [ ! -d "$kernelworkdir" ]; then
             $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
-        else
-
-else
-    echo "Using unknown kernel $kernelver"
-fi
 
 echo "don't forget to delete the .tar.xz"
 echo "end of program"
