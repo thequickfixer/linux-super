bebb05d9ece6e5d687fc90875e2ce6567602d5ae
insertname1180 <55028254+insertname1180@users.noreply.github.com>
2022-04-08 12:28:13 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,54 +1,58 @@
 #!/bin/bash
 
 # Variables
-ver=0.0.2
+input=""
+loginman=""
+kernelver=""
+
+# Other
+ver=0.0.3
 location=`pwd`
 kernelname=-super
-workdir=$location/linux-super-work
+WORKDIR=$location/linux-super-work
 kernelworkdir=/usr/src/linux-$kernelver
-
-# End of variables
 
 echo -ne "\nWelcome to the linux-super installer v$ver"
 
-echo -ne "\nPlease enter your preferred privilege escalation manager\n(doas or sudo)?\n" 
+while ! [ -x "$(command -v $loginman)" ]; do
+    echo -ne "\nPlease enter your preferred privilege escalation manager\n(doas or sudo)?\n" 
+    read -p "> " loginman
+done
 
-read -p "> " loginman
+mkdir linux-super-work
+cd $WORKDIR
+while [[ !(-f "linux-$kernelver.tar.xz") ]]; do #while the file does not exist on disk
+    echo -ne "\nWhat kernel version?\n(Note: 5.14.21 is the kernel default)\n" 
+    read -p "> " kernelver
+    wget --no-clobber https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$kernelver.tar.xz
+    if [[ !(-f "linux-$kernelver.tar.xz") ]]; then #tell the user something went wrong
+        echo "Could not find that version! Check for typos and try again."
+    fi
+done
+cd ..
 
-echo -ne "\nWhat kernel version?\n(Note: 5.14.21 is the kernel default)\n" 
 
-read -p "> " kernelver
 
-if [ $kernelver == 5.14.21 ]; then
-    echo "Download?"
-    read -p "> " _input
-else
-# if not 5.14.21 do:
-    echo "Attempting to download kernel $kernelver"
-    echo "Download?"
-    read -p "> " input
-	if [[ $input == y ]]; then
-        	mkdir linux-super-work
-        	cd $workdir
-        	wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$kernelver.tar.xz
-        	else
-		echo "user returned $input"
-        	fi
-fi
-    if [[ $_input == y ]]; then
-        mkdir linux-super-work
-        cd $workdir
-        wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$kernelver.tar.xz
-        else
-	echo "user returned $_input"
-	fi
-	if [ ! -d "$workdir/linux-$kernelver.tar.xz" ]; then
-            echo "no kernel version"
-	    fi
-	else
-        echo "Extract the files?"
-        if [ ! -d "$kernelworkdir" ]; then
-            $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
+#if [ $kernelver == "5.14.21" ]; then
+#   #TODO: apply 5.14.21-specific patches
+#fi
+
+#if [ $kernelver == "whatever" ]; then
+#   #TODO: apply whatever-specific patches
+#fi
+
+
+
+# Too scary to NOT have commented out
+#if [ ! -d "$workdir/linux-$kernelver.tar.xz" ]; then
+#    echo "no kernel version"
+#else
+#    echo "Extract the files?"
+#if [ ! -d "$kernelworkdir" ]; then
+#    $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
+#fi
+
+
 
 echo "don't forget to delete the .tar.xz"
 echo "end of program"
