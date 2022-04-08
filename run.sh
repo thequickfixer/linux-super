1058661986c94a59d9ee64e2042dd5c5a9b2b3e9
Bucket <64424124+thequickfixer@users.noreply.github.com>
2022-04-08 15:24:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,49 +15,32 @@ kernelworkdir=/usr/src/linux-$kernelver
 echo -ne "\nWelcome to the linux-super installer v$ver"
 
 while ! [ -x "$(command -v $loginman)" ]; do
-    echo -ne "\nPlease enter your preferred privilege escalation manager\n(doas or sudo)?\n" 
+    echo -ne "\nPlease enter your preferred privilege escalation manager\n(doas or sudo)?\n"
     read -p "> " loginman
 done
 
 mkdir linux-super-work
 cd $WORKDIR
 while [[ !(-f "linux-$kernelver.tar.xz") ]]; do #while the file does not exist on disk
-    echo -ne "\nWhat kernel version?\n(Note: 5.14.21 is the kernel default)\n" 
+    echo -ne "\nWhat kernel version?\n(Note: 5.14.21 is the kernel default)\n"
     read -p "> " kernelver
     wget --no-clobber https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$kernelver.tar.xz
     if [[ !(-f "linux-$kernelver.tar.xz") ]]; then #tell the user something went wrong
         echo "Could not find that version! Check for typos and try again."
     fi
 done
-if [ -d "$kernelworkdir" ]; then {
-    $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
-    cd $kernelworkdir
-    if [ $kernelver == "5.14.21" ]; then
-        #TODO: apply 5.14.21-specific patches
-        echo -ne "Applied 5.14.21 specific patches"
-    elif [ ! $kernelver == "5.14.21" ]; then
-        #APPLY GENERAL PATCHES (Hopefully it works lol)
-        echo -ne "Applied general patches"
-    fi
-}
 
-
-#if [ $kernelver == "whatever" ]; then
-#   #TODO: apply whatever-specific patches
-#fi
-
-
-
-# Too scary to NOT have commented out
-#if [ ! -d "$workdir/linux-$kernelver.tar.xz" ]; then
-#    echo "no kernel version"
-#else
-#    echo "Extract the files?"
-#if [ ! -d "$kernelworkdir" ]; then
-#    $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
-#fi
-
-
-
-echo "don't forget to delete the .tar.xz"
-echo "end of program"
+while ! [ -x "$(command -v $input)" ]; do
+    echo -ne "\nPerform extraction of linux-$kernelver\n(y or n)?\n"
+    read -p "> " input   
+    if 
+done
+$loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
+cd $kernelworkdir
+if [ $kernelver == "5.14.21" ]; then
+    #TODO: apply 5.14.21-specific patches
+    echo -ne "Applied 5.14.21 specific patches"
+elif [ $kernelver != "5.14.21" ]; then
+    #APPLY GENERAL PATCHES (Hopefully it works lol)
+    echo -ne "Applied general patches"
+fi
