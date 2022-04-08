95f1aa4ca56e482a289f7e801c1758c0948bdc50
Bucket <64424124+thequickfixer@users.noreply.github.com>
2022-04-07 22:33:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ver=0.0.1
-location=$pwd
+location=`pwd`
 
 echo
 echo "Welcome to the linux-super installer v$ver"
@@ -9,7 +9,6 @@ echo
 echo "Please enter your preferred login as manager (doas or sudo)"
 echo
 read -p "> " loginman
-echo 
+echo
 echo "Set preferred login as $loginman"
 echo
-echo $location
