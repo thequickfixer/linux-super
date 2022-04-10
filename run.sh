765c9d467e54ba97a6f8c6a084d68899f2df16fd
Bucket <64424124+thequickfixer@users.noreply.github.com>
2022-04-09 21:20:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -50,20 +50,21 @@ while [[ !(-f "linux-$kernelver.tar.xz") ]]; do #while the file does not exist o
     fi
 done
 
-if [ ! -d "/usr/src/linux-$kernelver" ]; then
-  echo -ne "\nPerform extraction of linux-$kernelver\n(y or n)?\n"
-  read -p "> " input
+while [ $inputdone != "true" ]; do
+    if [ ! -d "/usr/src/linux-$kernelver" ]; then
+    echo -ne "\nPerform extraction of linux-$kernelver\n(y or n)?\n"
+    read -p "> " input
     if [ $input == "y" ] || [ $input == "" ]; then
         echo -ne "\nPerforming extraction..."
         $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
+        inputdone="true"
     elif [ $input == "n" ]; then
         echo -ne "Exiting..\n"
+        inputdone="true"
         exit
     fi
-fi
-
-# reset input
-input=""
+done
+clr_input
 
 echo -ne "\nResuming this will:"
 echo -ne "\n- Add patches\n"
