aaf0fca6456756843bae2f1c7ec17b75e3c7f2cf
Bucket <64424124+thequickfixer@users.noreply.github.com>
2022-04-12 14:23:36 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -88,21 +88,8 @@ if [ ! -d "/etc/sysctl.d/override.conf" ]; then
 fi
 
 if [ ! -d "/usr/src/linux-$kernelver" ]; then
-    while [ $inputdone != "true" ]; do
-        echo -ne "\nPerform extraction of linux-$kernelver\n(y or n)?\n"
-        read -p "> " input
-        if [ $input == "y" ] || [ $input == "" ]; then
-            echo -ne "\nPerforming extraction..."
-            $loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/
-            inputdone="true"
-        elif [ $input == "n" ]; then
-            echo -ne "Exiting..\n"
-            inputdone="true"
-            exit
-        fi
-    done
+    get_input "\nPerform extraction of linux-$kernelver\n(y or n)?\n" "$loginman tar -xvf linux-$kernelver.tar.xz -C /usr/src/"
 fi
-clr_input
 
 echo -ne "\nResuming this will:"
 echo -ne "\n- Add patches\n"
