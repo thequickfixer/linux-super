20745d0e4895dfadbba399e10c8943f9a7b06130
Bucket <64424124+thequickfixer@users.noreply.github.com>
2022-04-17 15:44:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -163,6 +163,9 @@ read -p "Press enter to resume..."
 # TODO: force program to quit if ctrl-c below
 
 $loginman make $debug_make -j$physical_cpu_amount
-$loginman make modules_install && $loginman make install
+$loginman make modules_install
+#make install borked for a lot of operating systems.
+$loginman make install
+
 $loginman dracut --hostonly --no-compress --force --kver $kernelver-super
 $loginman grub-mkconfig -o /boot/grub/grub.cfg
